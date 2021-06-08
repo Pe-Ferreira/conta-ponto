768ff040466c66dcc3e9a700845b689c1ff2cad3
Pedro Ferreira <pe.ferreira@outlook.com>
2021-06-08 13:46:34 -0300

    solução para cross origin e backup

--- src/main/java/com/beardtech/contaponto/ContaPontoApplication.java
+++ src/main/java/com/beardtech/contaponto/ContaPontoApplication.java
@@ -1,5 +1,8 @@
 package com.beardtech.contaponto;
 
+import com.beardtech.contaponto.model.Amigo;
+import com.beardtech.contaponto.repo.AmigoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
